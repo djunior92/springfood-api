e62c1b0221444ec515a80d6ea8922f9cbe4722f4
djunior92 <juninho_2509@hotmail.com>
2021-10-01 21:59:23 -0300

    Propriedades customizadas com @Value

--- src/main/java/com/course/springfood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/course/springfood/di/notificacao/NotificadorEmail.java
@@ -1,14 +1,24 @@
 package com.course.springfood.di.notificacao;
 
 import com.course.springfood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + host);
+        System.out.println("Porta: " + porta);
+
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
