2212a540d17430e1fb5472b4539894d6c83ade6d
djunior92 <juninho_2509@hotmail.com>
2021-10-01 21:33:39 -0300

    Publicando e consumindo eventos customizados

--- src/main/java/com/course/springfood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/course/springfood/di/service/AtivacaoClienteService.java
@@ -1,23 +1,20 @@
 package com.course.springfood.di.service;
 
 import com.course.springfood.di.modelo.Cliente;
-import com.course.springfood.di.notificacao.NivelUrgencia;
-import com.course.springfood.di.notificacao.Notificador;
-import com.course.springfood.di.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
-    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
-    private Notificador notificador;
+    private ApplicationEventPublisher eventPublisher;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
     }
 
 }
