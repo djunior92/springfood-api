5ddad7db5b2b9a164af16f4c8929c03f4f0afdb9
djunior92 <juninho_2509@hotmail.com>
2021-10-03 15:31:36 -0300

    Manipulando a resposta HTTP com ResponseEntity

--- src/main/java/com/course/springfood/api/controller/CozinhaController.java
+++ src/main/java/com/course/springfood/api/controller/CozinhaController.java
@@ -4,11 +4,11 @@ import com.course.springfood.api.model.CozinhasXmlWrapper;
 import com.course.springfood.domain.model.Cozinha;
 import com.course.springfood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +19,12 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
     }
+
 
     @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
     public CozinhasXmlWrapper listarXml() {
@@ -30,8 +32,19 @@ public class CozinhaController {
     }
 
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId) {
-        return cozinhaRepository.buscar(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+//		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+//		return ResponseEntity.ok(cozinha);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .headers(headers)
+                .build();
     }
 
 }
