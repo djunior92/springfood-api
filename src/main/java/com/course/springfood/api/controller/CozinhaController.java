6105ed7e835d3e5e083ba588e797db2eb1eb4ed0
djunior92 <juninho_2509@hotmail.com>
2021-10-03 14:11:38 -0300

    Requisitando um Collection Resource com GET

---
+++ src/main/java/com/course/springfood/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.course.springfood.api.controller;
+
+import com.course.springfood.domain.model.Cozinha;
+import com.course.springfood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+
+}
