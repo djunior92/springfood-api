858e2f43f0e9e60128262da60f20a4db224fe6e8
djunior92 <juninho_2509@hotmail.com>
2021-10-03 14:59:08 -0300

    Customizando as representações XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName

--- src/main/java/com/course/springfood/domain/model/Cozinha.java
+++ src/main/java/com/course/springfood/domain/model/Cozinha.java
@@ -1,10 +1,13 @@
 package com.course.springfood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
+@JsonRootName("gastronomia")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -15,6 +18,8 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //	@JsonIgnore
+    @JsonProperty("titulo")
     @Column(nullable = false)
     private String nome;
 
