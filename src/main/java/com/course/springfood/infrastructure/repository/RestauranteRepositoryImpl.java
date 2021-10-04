1b32d21954214f8ed11b40a7912df23491abd5cd
djunior92 <juninho_2509@hotmail.com>
2021-10-04 16:48:48 -0300

    Repositório SDJ customizado

---
+++ src/main/java/com/course/springfood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.course.springfood.infrastructure.repository;
+
+import com.course.springfood.domain.model.Restaurante;
+import com.course.springfood.domain.repository.RestauranteRepositoryQueries;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Restaurante> find(String nome,
+                                  BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+
+        var jpql = "from Restaurante where nome like :nome "
+                + "and taxaFrete between :taxaInicial and :taxaFinal";
+
+        return manager.createQuery(jpql, Restaurante.class)
+                .setParameter("nome", "%" + nome + "%")
+                .setParameter("taxaInicial", taxaFreteInicial)
+                .setParameter("taxaFinal", taxaFreteFinal)
+                .getResultList();
+    }
+
+}
