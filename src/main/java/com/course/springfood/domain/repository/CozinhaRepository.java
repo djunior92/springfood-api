99dfd782925eac5b0207ce259967abdae6c14b04
djunior92 <juninho_2509@hotmail.com>
2021-10-04 12:21:14 -0300

    Consulta JPQL em repositório

--- src/main/java/com/course/springfood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/course/springfood/domain/repository/CozinhaRepository.java
@@ -1,13 +1,17 @@
 package com.course.springfood.domain.repository;
 
 import com.course.springfood.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface CozinhaRepository {
 
     List<Cozinha> listar();
+    List<Cozinha> consultarPorNome(String nome);
     Cozinha buscar(Long id);
+
     Cozinha salvar(Cozinha cozinha);
     void remover(Long id);
 
