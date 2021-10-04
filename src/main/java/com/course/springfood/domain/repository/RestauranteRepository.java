19aefcb6810a315e8413514fb72c620e04b554e2
djunior92 <juninho_2509@hotmail.com>
2021-10-04 13:19:57 -0300

    Consultas com query methods

--- src/main/java/com/course/springfood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/course/springfood/domain/repository/RestauranteRepository.java
@@ -4,7 +4,23 @@ import com.course.springfood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
+    List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+
+    Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+
+    List<Restaurante> findTop2ByNomeContaining(String nome);
+
+    int countByCozinhaId(Long cozinha);
+
 }
+
+
