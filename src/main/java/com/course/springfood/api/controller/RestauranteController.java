72c2c81e5273f2456f6db4be7360692d68fd6581
djunior92 <juninho_2509@hotmail.com>
2021-10-07 22:31:06 -0300

    Propriedades @CreationTimestamp e @UpdateTimestamp

--- src/main/java/com/course/springfood/api/controller/RestauranteController.java
+++ src/main/java/com/course/springfood/api/controller/RestauranteController.java
@@ -65,7 +65,7 @@ public class RestauranteController {
 
             if (restauranteAtual != null) {
                 BeanUtils.copyProperties(restaurante, restauranteAtual,
-                        "id", "formasPagamento");
+                        "id", "formasPagamento", "endereco", "dataCadastro");
 
                 restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
                 return ResponseEntity.ok(restauranteAtual);
--- src/main/java/com/course/springfood/domain/model/Restaurante.java
+++ src/main/java/com/course/springfood/domain/model/Restaurante.java
@@ -3,9 +3,12 @@ package com.course.springfood.domain.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,8 +32,19 @@ public class Restaurante {
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
+    @JsonIgnore
     @Embedded
     private Endereco endereco;
+
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataAtualizacao;
 
     @JsonIgnore
     @ManyToMany
