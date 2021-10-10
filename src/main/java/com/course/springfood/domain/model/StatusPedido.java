7a3a7f0f29b3f174761ad754960f3c3002c53f53
djunior92 <juninho_2509@hotmail.com>
2021-10-10 02:06:15 -0300

    Mapeando as entidades Pedido e ItemPedido

---
+++ src/main/java/com/course/springfood/domain/model/StatusPedido.java
@@ -0,0 +1,9 @@
+package com.course.springfood.domain.model;
+
+public enum StatusPedido {
+
+    CRIADO,
+    CONFIRMADO,
+    ENTREGUE,
+    CANCELADO
+}
--- src/main/java/com/course/springfood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/course/springfood/domain/repository/RestauranteRepository.java
@@ -1,7 +1,6 @@
 package com.course.springfood.domain.repository;
 
 import com.course.springfood.domain.model.Restaurante;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -33,4 +32,3 @@ public interface RestauranteRepository
     int countByCozinhaId(Long cozinha);
 
 }
-
