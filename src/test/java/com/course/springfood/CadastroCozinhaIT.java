ce385325166bc5659e23f5da8fced5cad8763fe8
djunior92 <juninho_2509@hotmail.com>
2021-10-16 16:26:50 -0300

    Restaurando o bd de testes para cada execução de teste com callback do Flyway

--- src/test/java/com/course/springfood/CadastroCozinhaIT.java
+++ src/test/java/com/course/springfood/CadastroCozinhaIT.java
@@ -2,9 +2,11 @@ package com.course.springfood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -20,11 +22,16 @@ public class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
     @Before
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+
+        flyway.migrate();
     }
 
     @Test
@@ -45,6 +52,18 @@ public class CadastroCozinhaIT {
                 .get()
                 .then()
                 .body("", hasSize(4));
+    }
+
+    @Test
+    public void testRetornarStatus201_QuandoCadastrarCozinha() {
+        given()
+                .body("{ \"nome\": \"Chinesa\" }")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
     }
 
 }
