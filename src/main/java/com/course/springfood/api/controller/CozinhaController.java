2fbe29f403d560f0a1b26fa406afbb98a809a675
Djalma Junior <65546799+djunior92@users.noreply.github.com>
2021-11-07 19:52:35 -0300

    Update CozinhaController.java

--- src/main/java/com/course/springfood/api/controller/CozinhaController.java
+++ src/main/java/com/course/springfood/api/controller/CozinhaController.java
@@ -10,7 +10,6 @@ import com.course.springfood.domain.service.CadastroCozinhaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
 
