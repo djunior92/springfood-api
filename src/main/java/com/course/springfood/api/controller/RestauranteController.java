40fcb18e4c07bedc7edc0494f27eeba2df6f546d
Djalma Junior <65546799+djunior92@users.noreply.github.com>
2021-11-08 18:08:28 -0300

    Update RestauranteController.java

--- src/main/java/com/course/springfood/api/controller/RestauranteController.java
+++ src/main/java/com/course/springfood/api/controller/RestauranteController.java
@@ -14,7 +14,6 @@ import com.course.springfood.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
 
