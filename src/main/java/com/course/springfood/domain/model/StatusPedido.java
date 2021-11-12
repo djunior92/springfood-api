23c1c80ebcbdfdc71abd7349ac276b6105cba0f6
djunior92 <juninho_2509@hotmail.com>
2021-11-11 21:37:25 -0300

    Implementando endpoints de transição de status de pedido

--- src/main/java/com/course/springfood/domain/model/StatusPedido.java
+++ src/main/java/com/course/springfood/domain/model/StatusPedido.java
@@ -2,8 +2,19 @@ package com.course.springfood.domain.model;
 
 public enum StatusPedido {
 
-    CRIADO,
-    CONFIRMADO,
-    ENTREGUE,
-    CANCELADO
+    CRIADO("Criado"),
+    CONFIRMADO("Confirmado"),
+    ENTREGUE("Entregue"),
+    CANCELADO("Cancelado");
+
+    private String descricao;
+
+    StatusPedido(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
 }
