d0ade1e8fc5d159925e4a6f095f476120b1e4349
djunior92 <juninho_2509@hotmail.com>
2021-12-09 00:17:39 -0300

    Selecionando a implementação do serviço de storage de fotos

--- src/main/java/com/course/springfood/core/storage/StorageConfig.java
+++ src/main/java/com/course/springfood/core/storage/StorageConfig.java
@@ -4,17 +4,23 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.course.springfood.core.storage.StorageProperties.TipoStorage;
+import com.course.springfood.domain.service.FotoStorageService;
+import com.course.springfood.infrastructure.service.storage.LocalFotoStorageService;
+import com.course.springfood.infrastructure.service.storage.S3FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AmazonS3Config {
+public class StorageConfig {
 
     @Autowired
     private StorageProperties storageProperties;
 
     @Bean
+    @ConditionalOnProperty(name = "springfood.storage.tipo", havingValue = "s3")
     public AmazonS3 amazonS3() {
         var credentials = new BasicAWSCredentials(
                 storageProperties.getS3().getIdChaveAcesso(),
@@ -24,6 +30,15 @@ public class AmazonS3Config {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(storageProperties.getS3().getRegiao())
                 .build();
+    }
+
+    @Bean
+    public FotoStorageService fotoStorageService() {
+        if (TipoStorage.S3.equals(storageProperties.getTipo())) {
+            return new S3FotoStorageService();
+        } else {
+            return new LocalFotoStorageService();
+        }
     }
 
 }
--- src/main/java/com/course/springfood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/course/springfood/infrastructure/service/storage/S3FotoStorageService.java
@@ -8,11 +8,9 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.course.springfood.core.storage.StorageProperties;
 import com.course.springfood.domain.service.FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.net.URL;
 
-@Service
 public class S3FotoStorageService implements FotoStorageService {
 
     @Autowired
