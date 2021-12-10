a34fc328640314b458a1b440483122768857ceff
djunior92 <juninho_2509@hotmail.com>
2021-12-09 23:08:20 -0300

    Implementando envio de e-mails transacionais com SendGrid

--- src/main/java/com/course/springfood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/course/springfood/domain/service/FluxoPedidoService.java
@@ -1,6 +1,7 @@
 package com.course.springfood.domain.service;
 
 import com.course.springfood.domain.model.Pedido;
+import com.course.springfood.domain.service.EnvioEmailService.Mensagem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +12,22 @@ public class FluxoPedidoService {
     @Autowired
     private EmissaoPedidoService emissaoPedido;
 
+    @Autowired
+    private EnvioEmailService envioEmail;
+
     @Transactional
     public void confirmar(String codigoPedido) {
         Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
         pedido.confirmar();
+
+        var mensagem = Mensagem.builder()
+                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+                .corpo("pedido-confirmado.html")
+                .variavel("pedido", pedido)
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+
+        envioEmail.enviar(mensagem);
     }
 
     @Transactional
