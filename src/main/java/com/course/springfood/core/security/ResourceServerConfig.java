1012a62404849eda7af587ed91196f49431a66e1
djunior92 <juninho_2509@hotmail.com>
2022-01-03 20:15:22 -0300

    Configurando a validação de JWT com a chave pública

--- src/main/java/com/course/springfood/core/security/ResourceServerConfig.java
+++ src/main/java/com/course/springfood/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package com.course.springfood.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-
-import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -22,13 +17,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .cors().and()
                 .oauth2ResourceServer().jwt();
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        var secretKey = new SecretKeySpec("YXV0aG9yaXphdGlvbmRqYWxtYXNwcmluZ2Zvb2Q".getBytes(), "HmacSHA256");
-
-        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 
 }
